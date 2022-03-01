7f70c9a3d6e3f31174fea1cacbd4608f8e9480d9
Danh Nguyen <danh.nguyenvan@asnet.com.vn>
2022-03-01 09:37:47 +0700

    Add exercise 11

---
+++ exercise/exercise11/exercise11-2.py
@@ -0,0 +1,12 @@
+a = {'a': 1, 'b': 2, 'c': 1, 'd': 4, 'e':1}
+def invert_dict(d):
+  inverse = dict()
+  for key in d:
+    val = d[key]
+    inverse.setdefault(val, []).append(key)
+  return inverse
+
+print(invert_dict(a))
+
+
+
