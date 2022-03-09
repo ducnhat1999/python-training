8a7f13c8ee9e4718e41340e2f46c59d2fdcac9a5
Danh Nguyen <danh.nguyenvan@asnet.com.vn>
2022-03-09 14:15:03 +0700

    Add exercise chapter 18

--- exercise/exercises-18/app.py
+++ exercise/exercises-18/app.py
@@ -1,5 +1,6 @@
 import random
 from re import S
+from sys import prefix
 
 class Card:
   suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
@@ -50,6 +51,22 @@ class Hand(Deck):
   def __init__(self, label=""):
     self.cards = []
     self.label = label
+
+class Markov:
+  def __init__(self):
+    self.suffix_map = {}
+    self.prefix = ()
+  def process_word(self, word, order=2):
+    if len(self.prefix) < order:
+      self.prefix += (word,)
+      return
+
+    try:
+      self.suffix_map[self.prefix].append(word)
+    except KeyError:
+      self.suffix_map[self.prefix] = [word]
+
+    self.prefix = shift(self.prefix, word)
 
 
 queen_of_diamonds = Card(1, 12)
